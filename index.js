710e4186f6c074f7463cd3971e310d3dc27a74b6
anagha321 <anaghas1004@gamil.com>
2025-07-27 16:13:50 +0530

    Final

--- index.js
+++ index.js
@@ -9,33 +9,33 @@ let playerScore = 0;
 let computerScore = 0;
 
 
-function playGame(playerChoice){
+function playGame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     let result = "";
 
-    if(playerChoice === computerChoice){
+    if (playerChoice === computerChoice) {
         result = "IT'S A TIE !";
     }
-    else{
-        switch(playerChoice){
+    else {
+        switch (playerChoice) {
             case "rock":
-               result =  (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
-               break;
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
             case "paper":
-               result =  (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
-               break;
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
             case "scissors":
-               result =  (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
-               break;
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
         }
     }
 
     playerDisplay.textContent = `PLAYER : ${playerChoice}`;
-    computerDisplay.textContent = `PLAYER : ${computerChoice}`;
+    computerDisplay.textContent = `COMPUTER : ${computerChoice}`;
     resultDisplay.textContent = result;
 
     resultDisplay.classList.remove("greenText", "redText");
-    switch(result){
+    switch (result) {
         case "YOU WIN!":
             resultDisplay.classList.add("greenText");
             playerScore++;
